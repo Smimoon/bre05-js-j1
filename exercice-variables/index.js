8dc142097372dc1eea34563a91686978baf7536e
smimoon <simonlaroche@orange.fr>
2026-01-06 09:37:57 +0000

    Save after finishing the exercise

---
+++ exercice-variables/index.js
@@ -0,0 +1,16 @@
+const character = "legolas";
+console.log (character);
+let name ="legolas"
+console.log (name);
+name="gimli";
+console.log(name);
+let x = "1234567898765432123456789098767656787654545432334567543145542";
+console.log(x);
+let y = "0,67857856987981708678526735297186089126782578156189639016301";
+console.log(y);
+let z = "-5676";
+console.log(z);
+let yes = true;
+let no = false;
+console.log(yes);
+console.log(no);
