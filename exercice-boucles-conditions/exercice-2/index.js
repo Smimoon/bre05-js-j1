a6fa77b72098ddcb35a1b18cb33903edd890cbbb
smimoon <simonlaroche@orange.fr>
2026-01-06 13:17:06 +0000

    Save after adding code to index.js and finishing the exercise

---
+++ exercice-boucles-conditions/exercice-2/index.js
@@ -0,0 +1,13 @@
+let i = 0;
+
+while(i<=10) {
+    console.log(i);
+    if (i%2===0) {
+        console.log("pair");
+        
+    }
+    else {
+        console.log("impair");
+    }
+    i=i+1;
+}
